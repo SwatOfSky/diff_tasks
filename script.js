e87cc13bd7e9a9930717603b8a64c43f7b87eee4
Radionov Dmytro <d1ma999@i.ua>
2022-02-05 20:35:51 +0200

    'reversedArray_finished'

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //сумма квадратных корней всех четных чисел
-
 console.log(
     [1, 4, 3, 16, 4, 256, 4]
       .filter(element => !(element % 2))
@@ -8,4 +7,11 @@ console.log(
 
 //заполняет массив значениями
 const fill = (arraySize, value) => new Array(arraySize).fill(value);
-console.log(fill(5, 's'))
+console.log(fill(5, 's'));
+
+//функция, которая разворачивает массив в обратном порядке
+console.log(
+[1, 2, 3]
+.map((el, id, givenArr) => givenArr[givenArr.length - id - 1])
+)
+
